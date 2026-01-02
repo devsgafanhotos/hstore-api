287b47999e764ec703b903c66e989955667cc7d5
Heremenegildo Wilson <hermenegildowilson7@gmail.com>
2026-01-02 15:54:46 +0100

    Remove fallback para conexão local no Sequelize, utilizando apenas DATABASE_URL

--- config/database.js
+++ config/database.js
@@ -3,15 +3,7 @@ const Sequelize = require("sequelize");
 
 // Use diretamente a URL de conexão completa, se estiver no Railway
 const sequelize = new Sequelize(
-    process.env.DATABASE_URL || {
-        // fallback local
-        database: process.env.DB_NAME,
-        username: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT,
-        dialect: "mysql",
-    }
+    process.env.DATABASE_URL
 );
 
 module.exports = { sequelize, Sequelize };
